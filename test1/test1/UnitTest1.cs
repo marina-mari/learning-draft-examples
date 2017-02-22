1cfc7117f88576766e1ade302a21c67d9e5a6001
marina-mari <mira.sopina1@gmail.com>
2017-02-22 21:32:41 +0200

    First commit

---
+++ test1/test1/UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+namespace UnitTestProject1
+{
+    [TestFixture]
+    public class MyFirstTest
+    {
+        private IWebDriver driver;
+
+        [SetUp]
+        public void start()
+        {
+            driver = new ChromeDriver();
+        }
+        [Test]
+        public void FirstTest()
+        {
+            driver.Url = "http://google.com";
+           
+        }
+
+        [TearDown]
+        public void stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+
+
+    }
+}
---
+++ test1/test1/UnitTest2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace test1
+{
+    [TestFixture]
+    public class UnitTest2
+    {
+        private IWebDriver driver;
+        [SetUp]
+
+        public void start()
+        {
+            driver = new ChromeDriver();
+        }
+        [Test]
+        public void test2()
+        {
+            driver.Url = "http://localhost/litecart/admin/";
+            var username = driver.FindElement(By.Name("username"));
+            username.SendKeys("admin");
+            var password = driver.FindElement(By.Name("password"));
+            password.SendKeys("admin");
+            var submit = driver.FindElement(By.TagName("button"));
+            submit.Submit();
+        }
+
+        [TearDown]
+        public void stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
