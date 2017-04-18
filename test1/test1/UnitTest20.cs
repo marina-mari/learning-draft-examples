dc0cc4231bc9873828a656b3ca14dfabb3eab3e5
marina-mari <mira.sopina1@gmail.com>
2017-04-18 11:35:17 +0300

    add screenshot capture and saving it on FS

--- test1/test1/UnitTest20.cs
+++ test1/test1/UnitTest20.cs
@@ -2,6 +2,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.Events;
 using NUnit.Framework;
 using NUnit;
 using System.Collections.Generic;
@@ -18,15 +19,25 @@ namespace test1
 	public class UnitTest20
 	{
 
-		IWebDriver driver;
+		EventFiringWebDriver driver;
 		[SetUp]
 		public void Start()
 		{
-			driver = new ChromeDriver();
+			driver = new EventFiringWebDriver( new ChromeDriver());
+			//driver.FindingElement += Driver_FindingElement;
+			driver.FindingElement += (sender, e) => Console.WriteLine(e.FindMethod);
+			driver.FindElementCompleted += (sender, e) => Console.WriteLine(e.FindMethod + "element is found");
+			driver.ExceptionThrown += (sender, e) => Console.WriteLine(e.ThrownException);
 			driver.Url = "http://localhost:8080/litecart/admin/?app=catalog&doc=catalog&category_id=1";
 			driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
 			auth(driver);
 		}
+
+		//
+		//private void Driver_FindingElement(object sender, FindElementEventArgs e)
+		//{
+		//	Console.WriteLine(e.FindMethod);
+		//}
 
 		public void auth(IWebDriver driver)
 		{
@@ -49,6 +60,16 @@ namespace test1
 				System.Threading.Thread.Sleep(2000);
 				AllDucks = driver.FindElements(By.XPath("//*[@class='dataTable']//td/a[contains (., 'Duck')]"));
 				AllDucks[i].Click();
+				driver.GetScreenshot().SaveAsFile("C:\\Resources\\screen", ScreenshotImageFormat.Png);
+
+				//String strAppPath = Path.GetDirectoryName(System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName);
+				//Console.WriteLine(strAppPath);
+				//String strFilePath = Path.Combine(strAppPath, "Resources");
+				//Console.WriteLine(strFilePath);
+				//String strFullFilename = Path.Combine(strFilePath, "screen.png");
+				//Console.WriteLine(strFullFilename);
+				Console.WriteLine(driver.Manage().Logs.AvailableLogTypes);
+				Console.WriteLine(driver.Manage().Logs.GetLog("browser"));
 				System.Diagnostics.Trace.Write(driver.Manage().Logs.GetLog("browser"));
 
 				
